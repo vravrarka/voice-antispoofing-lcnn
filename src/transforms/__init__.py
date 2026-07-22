1f9e89706bbdf72f074868755348f9da5d617910
vravrarka <130647301+vravrarka@users.noreply.github.com>
2026-07-22 18:13:12 +0300

    Update __init__.py

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,2 +1,9 @@
 from src.transforms.normalize import Normalize1D
 from src.transforms.scale import RandomScale1D
+from src.transforms.spectrogram import LogPowerSpectrogram
+
+__all__ = [
+    "Normalize1D",
+    "RandomScale1D",
+    "LogPowerSpectrogram",
+]
