c7376ab651bd9ed0998571ec0f6c9d2901e21919
vravrarka <130647301+vravrarka@users.noreply.github.com>
2026-07-22 13:58:13 +0300

    Update collate.py

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,25 +1,23 @@
 import torch
 
 
-def collate_fn(dataset_items: list[dict]):
-    """
-    Collate and pad fields in the dataset items.
-    Converts individual items into a batch.
-
-    Args:
-        dataset_items (list[dict]): list of objects from
-            dataset.__getitem__.
-    Returns:
-        result_batch (dict[Tensor]): dict, containing batch-version
-            of the tensors.
-    """
-
+def collate_fn(dataset_items: list[dict]) -> dict:
+    
     result_batch = {}
 
     # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
+    result_batch["features"] = torch.stack(
+        [
+            elem["features"] 
+            for elem in dataset_items
+        ],
+        dim=0,
     )
-    result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
+    result_batch["labels"] = torch.tensor(
+        [
+            elem["labels"] 
+            for elem in dataset_items
+        ]
+    )
 
     return result_batch
