fb4ea7745a1172fa606e54862ccd0778d0e121ac
vravrarka <130647301+vravrarka@users.noreply.github.com>
2026-07-23 16:26:09 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import warnings
-
 import hydra
 import torch
 from hydra.utils import instantiate
@@ -14,46 +13,23 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 @hydra.main(version_base=None, config_path="src/configs", config_name="lcnn_baseline")
 def main(config):
-    """
-    Main script for training. Instantiates the model, optimizer, scheduler,
-    metrics, logger, writer, and dataloaders. Runs Trainer to train and
-    evaluate the model.
-
-    Args:
-        config (DictConfig): hydra experiment config.
-    """
     set_random_seed(config.trainer.seed)
-
     project_config = OmegaConf.to_container(config)
     logger = setup_saving_and_logging(config)
     writer = instantiate(config.writer, logger, project_config)
-
     if config.trainer.device == "auto":
         device = "cuda" if torch.cuda.is_available() else "cpu"
     else:
         device = config.trainer.device
-
-    # setup data_loader instances
-    # batch_transforms should be put on device
     dataloaders, batch_transforms = get_dataloaders(config, device)
-
-    # build model architecture, then print to console
     model = instantiate(config.model).to(device)
     logger.info(model)
-
-    # get function handles of loss and metrics
     loss_function = instantiate(config.loss_function).to(device)
     metrics = instantiate(config.metrics)
-
-    # build optimizer, learning rate scheduler
     trainable_params = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = instantiate(config.optimizer, params=trainable_params)
     lr_scheduler = instantiate(config.lr_scheduler, optimizer=optimizer)
-
-    # epoch_len = number of iterations for iteration-based training
-    # epoch_len = None or len(dataloader) for epoch-based training
     epoch_len = config.trainer.get("epoch_len")
-
     trainer = Trainer(
         model=model,
         criterion=loss_function,
@@ -69,9 +45,7 @@ def main(config):
         batch_transforms=batch_transforms,
         skip_oom=config.trainer.get("skip_oom", True),
     )
-
     trainer.train()
-
 
 if __name__ == "__main__":
     main()
