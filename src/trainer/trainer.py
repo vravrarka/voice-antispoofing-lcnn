8a9ee1e5419f7dba7323b25b834735f676dd496a
vravrarka <130647301+vravrarka@users.noreply.github.com>
2026-07-23 16:18:37 +0300

    Update trainer.py

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -6,9 +6,6 @@ from src.trainer.base_trainer import BaseTrainer
 
 
 class Trainer(BaseTrainer):
-    """
-    Trainer class. Defines the logic of batch logging and processing.
-    """
     def _evaluation_epoch(
         self,
         epoch,
@@ -105,47 +102,23 @@ class Trainer(BaseTrainer):
         return logs
 
     def process_batch(self, batch, metrics: MetricTracker):
-        """
-        Run batch through the model, compute metrics, compute loss,
-        and do training step (during training stage).
-
-        The function expects that criterion aggregates all losses
-        (if there are many) into a single one defined in the 'loss' key.
-
-        Args:
-            batch (dict): dict-based batch containing the data from
-                the dataloader.
-            metrics (MetricTracker): MetricTracker object that computes
-                and aggregates the metrics. The metrics depend on the type of
-                the partition (train or inference).
-        Returns:
-            batch (dict): dict-based batch containing the data from
-                the dataloader (possibly transformed via batch transform),
-                model outputs, and losses.
-        """
         batch = self.move_batch_to_device(batch)
         batch = self.transform_batch(batch)  # transform batch on device -- faster
-
         metric_funcs = self.metrics["inference"]
         if self.is_train:
             metric_funcs = self.metrics["train"]
             self.optimizer.zero_grad()
-
         outputs = self.model(**batch)
         batch.update(outputs)
-
         all_losses = self.criterion(**batch)
         batch.update(all_losses)
-
         if self.is_train:
             batch["loss"].backward()  # sum of all losses is always called loss
             self._clip_grad_norm()
             self.optimizer.step()
-
         batch_size = int(
             batch["labels"].shape[0]
         )
-        # update metrics for each loss (in case of multiple losses)
         for loss_name in self.config.writer.loss_names:
             metrics.update(loss_name, batch[loss_name].item(), n=batch_size,)
         if self.is_train:
@@ -153,30 +126,12 @@ class Trainer(BaseTrainer):
                 batch["loss"].item() * batch_size
             )
             self._epoch_train_examples += batch_size
-
         for met in metric_funcs:
             metrics.update(met.name, met(**batch), n=batch_size,)
         return batch
 
     def _log_batch(self, batch_idx, batch, mode="train"):
-        """
-        Log data from batch. Calls self.writer.add_* to log data
-        to the experiment tracker.
-
-        Args:
-            batch_idx (int): index of the current batch.
-            batch (dict): dict-based batch after going through
-                the 'process_batch' function.
-            mode (str): train or inference. Defines which logging
-                rules to apply.
-        """
-        # method to log data from you batch
-        # such as audio, text or images, for example
-
-        # logging scheme might be different for different partitions
-        if mode == "train":  # the method is called only every self.log_step steps
-            # Log Stuff
+        if mode == "train":  
             pass
         else:
-            # Log Stuff
             pass
