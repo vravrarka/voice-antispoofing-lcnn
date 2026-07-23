f04d775a0718f02b15d40bcd53a847b32cb8c6cd
vravrarka <130647301+vravrarka@users.noreply.github.com>
2026-07-23 16:19:32 +0300

    Update scale.py

--- src/transforms/scale.py
+++ src/transforms/scale.py
@@ -3,21 +3,10 @@ from torch import nn
 
 
 class RandomScale1D(nn.Module):
-    """
-    Randomly Scale 1D Input. Dummy Example of an augmentation.
-    Used as an instance transform.
-    """
-
     def __init__(self):
         super().__init__()
 
     def forward(self, x):
-        """
-        Args:
-            x (Tensor): input tensor.
-        Returns:
-            x (Tensor): scaled tensor.
-        """
         scale = torch.randn(1)
         x = scale * x
         return x
